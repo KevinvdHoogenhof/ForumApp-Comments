f24bef929a643bd2b5da7401e2a4feb77d06c1e6
Kevin <k.vandenhoogenhof@student.fontys.nl>
2024-04-13 10:18:54 +0200

    Update CommentService.cs
    Use the comment context instead of making database calls directly

--- CommentService.API/Services/CommentService.cs
+++ CommentService.API/Services/CommentService.cs
@@ -1,13 +1,45 @@
-﻿using CommentService.API.Models;
+﻿using CommentService.API.Context;
+using CommentService.API.Models;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Options;
 using MongoDB.Driver;
+using System.Xml.Linq;
 
 namespace CommentService.API.Services
 {
     public class CommentService : ICommentService
     {
-        private readonly IMongoCollection<Comment> _comments;
+        private readonly ICommentContext _context;
+        public CommentService(ICommentContext context)
+        {
+            _context = context;
+        }
+        public async Task<Comment?> GetComment(string id)
+        {
+            return await _context.GetAsync(id);
+        }
+
+        public async Task<List<Comment>> GetComments()
+        {
+            return await _context.GetAsync();
+        }
+
+        public async Task InsertComment(Comment comment)
+        {
+            await _context.CreateAsync(comment);
+        }
+
+        public async Task UpdateComment(Comment comment)
+        {
+            await _context.UpdateAsync(comment);
+        }
+
+        public async Task DeleteComment(string id)
+        {
+            await _context.RemoveAsync(id);
+        }
+
+        /*private readonly IMongoCollection<Comment> _comments;
         public CommentService(IOptions<CommentDBSettings> settings)
         {
             var mongoClient = new MongoClient(settings.Value.ConnectionString);
@@ -37,6 +69,6 @@ namespace CommentService.API.Services
         public async Task DeleteComment(string id)
         {
             await _comments.DeleteOneAsync(x => x.Id == id);
-        }
+        }*/
     }
 }
