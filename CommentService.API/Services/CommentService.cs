39e63e03c0703bd723d3f5f51bf338e20f7c3c97
Kevin <k.vandenhoogenhof@student.fontys.nl>
2024-04-13 10:19:17 +0200

    Update CommentService.cs

--- CommentService.API/Services/CommentService.cs
+++ CommentService.API/Services/CommentService.cs
@@ -38,37 +38,5 @@ namespace CommentService.API.Services
         {
             await _context.RemoveAsync(id);
         }
-
-        /*private readonly IMongoCollection<Comment> _comments;
-        public CommentService(IOptions<CommentDBSettings> settings)
-        {
-            var mongoClient = new MongoClient(settings.Value.ConnectionString);
-            var mongoDatabase = mongoClient.GetDatabase(settings.Value.DatabaseName);
-            _comments = mongoDatabase.GetCollection<Comment>(settings.Value.CollectionName);
-        }
-        public async Task<Comment?> GetComment(string id)
-        {
-            return await _comments.Find(x => x.Id == id).FirstOrDefaultAsync();
-        }
-
-        public async Task<List<Comment>> GetComments()
-        {
-            return await _comments.Find(_ => true).ToListAsync();
-        }
-
-        public async Task InsertComment(Comment comment)
-        {
-            await _comments.InsertOneAsync(comment);
-        }
-
-        public async Task UpdateComment(Comment comment)
-        {
-            await _comments.ReplaceOneAsync(x => x.Id == comment.Id, comment);
-        }
-
-        public async Task DeleteComment(string id)
-        {
-            await _comments.DeleteOneAsync(x => x.Id == id);
-        }*/
     }
 }
